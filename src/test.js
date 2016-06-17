8dd9fd871fd7369754ef961dd18320e665160fa4
Volodymyr V. Diachenko <diachenko.vladimir@gmail.com>
2016-06-17 13:32:19 +0300

    Added some names. Created test.js

---
+++ src/test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by vinside on 6/17/16.
+ */
+var lib = require("./index");
+
+console.log(lib.all);
+console.log(lib.random());
+console.log(lib.random());
+console.log(lib.random());
+console.log(lib.random());
