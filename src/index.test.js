49fe64556be6f0c7c0cf4c81cc618c599917db42
Volodymyr V. Diachenko <diachenko.vladimir@gmail.com>
2016-06-17 17:06:53 +0300

    chore(releasing): Add travis config, conentional  commit, and semantic-release
    closes #1

---
+++ src/index.test.js
@@ -0,0 +1,31 @@
+/**
+ * Created by vinside on 6/17/16.
+ */
+var expect = require('chai').expect;
+var starWars = require('./index');
+
+describe('starwars-names', function() {
+  describe('all', function () {
+    it('should be an array of strings', function () {
+      expect(starWars.all).to.not.satisfy(isArrayOfStrings);
+
+      function isArrayOfStrings(array) {
+        return array.every(function(item) {
+          return typeof item === 'object';
+        });
+      }
+    });
+
+    it('should contain "Some Some" ', function () {
+      expect(starWars.all).to.include('Some Some');
+    });
+  });
+
+  describe('random', function () {
+    it('should return a random item from the starWars.all', function () {
+      var randomItem = starWars.random();
+      expect(starWars.all).to.include(randomItem);
+    });
+  });
+
+});
