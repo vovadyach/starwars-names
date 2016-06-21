e2c2ec5b79ed1df0735f260fd5cf4a7f86681de1
Volodymyr V. Diachenko <diachenko.vladimir@gmail.com>
2016-06-21 11:06:32 +0300

    Added check-coverag script ot package.json

--- src/index.test.js
+++ src/index.test.js
@@ -32,9 +32,7 @@ describe('starwars-names', function() {
       expect(randomItems).to.have.length(3);
       randomItems.forEach(function (item) {
         expect(starWars.all).to.include(item);
-
       });
     });
   });
-
 });
